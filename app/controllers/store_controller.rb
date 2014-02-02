ed3e3192892102515faa21318a2aebcecc5c76e8
Ryan Robinson <ryan.macmaster.robinson@gmail.com>
2014-02-02 11:44:47 -0500

    After completing Task D, cart generation.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,5 +2,10 @@ class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
     @time = Time.now.to_formatted_s(:short)
+    if session[:counter].nil?
+      session[:counter] = 1
+    else
+      session[:counter] += 1
+    end
   end
 end
